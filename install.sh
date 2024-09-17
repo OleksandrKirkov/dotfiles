d95cc3848b991fed67a8ad7845ecc93d1af5de50
OleksandrKirkov <kirkovsasha009@gmail.com>
2024-09-17 03:13:25 +0300

    chore(install.sh): delete konsole

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 sudo apt update
+sudo apt-get update
 
-sudo apt install neovim python3-neovim konsole git
+sudo apt-get install build-essential
+
+sudo apt install neovim python3-neovim git nodejs \
+    npm
 
 sudo snap install telegram-desktop spotify
+
+git clone https://github.com/OleksandrKirkov/nvim.git ~/.config
