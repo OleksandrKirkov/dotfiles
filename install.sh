8443728f114009ee0b983eb67ad3fa5c0747cea1
OleksandrKirkov <kirkovsasha009@gmail.com>
2024-09-17 04:31:50 +0300

    feat(install.sh): add zsh and add other plugins for c#

--- install.sh
+++ install.sh
@@ -3,11 +3,17 @@
 sudo apt update
 sudo apt-get update
 
-sudo apt-get install build-essential
+sudo apt-get install -y build-essential
 
-sudo apt install neovim python3-neovim git nodejs \
-    npm
+sudo apt install -y neovim python3-neovim git nodejs \
+    npm curl zsh dotnet-sdk-8.0 aspnetcore-runtime-8.0 \
+    dotnet-runtime-8.0 zlib1g 
 
 sudo snap install telegram-desktop spotify
 
 git clone https://github.com/OleksandrKirkov/nvim.git ~/.config
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+wget https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list
+sudo cp prod.list /etc/apt/sources.list.d/mssql-release.list
